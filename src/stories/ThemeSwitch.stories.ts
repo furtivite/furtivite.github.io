e5350a2fe395e117230121cb1d508b8efb4d9f6a
Egor Levchenko <50330458+furtivite@users.noreply.github.com>
2024-08-02 17:18:27 +0300

    [JSX + CSS]: Создаёт компонент Header в сторибук (#5)
    * Удаляет ненужное
    
    * Добавляет компонент кнопки переключения темы
    
    * Добавляет компонент header
    
    * Добавил тип
    
    * Правки после ревью

---
+++ src/stories/ThemeSwitch.stories.ts
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { ThemeSwitchBtn } from './ThemeSwitch';
+
+type Story = StoryObj<typeof meta>;
+
+const meta: Meta<typeof ThemeSwitchBtn> = {
+  title: 'Example/LightModeBtn',
+  component: ThemeSwitchBtn,
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+export const ThemeSwitchBtnCode: Story = {};
