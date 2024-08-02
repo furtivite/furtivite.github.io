b43c012bb83fec261f7d4f7259bfde83f50c4229
Nikolai Konyushkov <54687130+parabutteo@users.noreply.github.com>
2024-08-02 18:49:29 +0300

    [JSX + CSS]: Добавляет короткую карточку операции в сторибук (#7)
    Co-authored-by: @konyushkov.nk <konyushkov.nk@esphere.ru>

---
+++ src/stories/ShortCard.stories.ts
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { ShortCard } from './ShortCard';
+
+type Story = StoryObj<typeof meta>;
+
+const meta: Meta<typeof ShortCard> = {
+  title: 'Example/ShortCard',
+  component: ShortCard,
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+export const ShortCardCode: Story = {
+  args: {
+    title: 'АЗС',
+    categoryName: 'Транспорт',
+    price: 2500,
+    description:
+      'Активное развитие ресурсной базы, использование самых современных технологий добычи и переработки нефти, наличие эффективной сбытовой сети позволяют «Газпром нефти» уверенно занимать позицию одного из лидеров российской нефтегазовой отрасли и играть заметную роль на мировом нефтегазовом рынке.',
+  },
+};
---
+++ src/stories/ShortCard.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import './short-card.css';
+
+interface ShortCardProps {
+  categoryName: 'Транспорт' | 'Продукты';
+  title: string;
+  price: number;
+  description: string;
+}
+
+/**
+ * Компонет логотипа
+ */
+export const ShortCard = ({ categoryName, title, price, description }: ShortCardProps): React.ReactElement => (
+  <article className="card">
+    <header className="flex">
+      <div className="flex">
+        <div className="category">
+          <img
+            width="20"
+            height="20"
+            src={
+              categoryName === 'Транспорт'
+                ? 'https://img.icons8.com/ios-filled/20/5b7cfa/car.png'
+                : 'https://img.icons8.com/ios-filled/20/5b7cfa/fast-moving-consumer-goods.png'
+            }
+            alt=""
+          />
+          <span>{categoryName}</span>
+        </div>
+        <h1>{title}</h1>
+      </div>
+      <p>
+        {price}
+        <span>₽</span>
+      </p>
+    </header>
+    <p>{description.slice(0, 100).slice(0, -3) + '...'}</p>
+  </article>
+);
