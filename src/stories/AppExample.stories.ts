1cf20efaea9956c5071e9a1a87d579dd87b3a58b
@konyushkov.nk <konyushkov.nk@esphere.ru>
2024-08-06 17:44:04 +0300

    Правит сторю с примером приложения

--- src/stories/AppExample.stories.ts
+++ src/stories/AppExample.stories.ts
@@ -5,17 +5,11 @@ import { AppExample as AppExampleComponent } from './AppExample';
 type Story = StoryObj<typeof meta>;
 
 const meta: Meta<typeof AppExampleComponent> = {
-  title: 'Example/AppExampleComponent',
+  title: 'App/AppExampleComponent',
   component: AppExampleComponent,
   tags: ['autodocs'],
 };
 
 export default meta;
 
-export const Btn: Story = {
-  args: {
-    text: 'Кнопка',
-    onClick: () => console.log('КЛИК'),
-    isDisabled: false,
-  },
-};
+export const AppExample: Story = {};
