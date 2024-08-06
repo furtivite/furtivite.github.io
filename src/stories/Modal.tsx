38c697dfbb58f2ebb5cb52cd3b819c4f14644b00
Nikolai Konyushkov <54687130+parabutteo@users.noreply.github.com>
2024-08-06 09:54:21 +0300

    Добавляет модалку в сторибук (#10)
    Co-authored-by: @konyushkov.nk <konyushkov.nk@esphere.ru>

---
+++ src/stories/Modal.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Btn } from './Btn';
+
+import './modal.css';
+
+interface ModalComponentProps {
+  /** Признак видимости компонента */
+  showModal: boolean;
+  /** Обработчик закрытия */
+  closeHandler: () => void;
+  /** Заголовок */
+  title: string;
+  /** Тело */
+  children: React.ReactNode;
+  /** Целевая кнопка */
+  titleButton: string;
+  /** Обработчик целевой кнопки */
+  onClickButton: () => void;
+}
+
+/**
+ * Компонент "Модальное окно"
+ */
+export const Modal = ({
+  showModal,
+  closeHandler,
+  title,
+  children,
+  titleButton,
+  onClickButton,
+}: ModalComponentProps): React.ReactElement => {
+  if (!showModal) return null;
+
+  return (
+    <div className="modal-wrapper">
+      <div className="modal-window">
+        <button className="modal-close" onClick={closeHandler} />
+        <h1 className="modal-header">{title}</h1>
+        <div className="modal-body">{children}</div>
+        <div className="modal-footer">
+          <Btn text={titleButton} onClick={onClickButton} />
+        </div>
+      </div>
+    </div>
+  );
+};
