f60b5402170253a8c70660ca465e8f894c31887f
Egor Levchenko <antytoto@gmail.com>
2024-12-06 11:24:49 +0300

    feat: make about page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,7 +8,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Текст писать тут
+          Привет, меня зовут Егор. Я&nbsp;&mdash; разработчик интерфейсов.
+          <br />
+          Пишу на React, Angular с TypeScript. Хочу разобраться с React получше.
+          <br />
+          Контакт{' '}
+          <a href="http://t.me/antytoto" target="_blank" rel="noopener noreferrer" style={{ color: 'white' }}>
+            @antytoto
+          </a>
         </p>
       </header>
     </div>
