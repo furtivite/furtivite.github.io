bd1f698b969103d0242685c6d91bebce9cd017c4
Egor Levchenko <antytoto@gmail.com>
2024-12-24 09:42:26 +0300

    feat: create logo

---
+++ src/stories/Logo.stories.ts
@@ -0,0 +1,32 @@
+import type { Meta } from '@storybook/react';
+import { Logo } from '../components';
+import { ELogoType } from '../components/interfaces';
+
+const meta: Meta<typeof Logo> = {
+  title: 'UI/Logo',
+  component: Logo,
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+export const Dark = {
+  args: {
+    type: ELogoType.DARK,
+    text: 'Ecommerce',
+  },
+};
+
+export const Light = {
+  args: {
+    type: ELogoType.LIGHT,
+    text: 'Ecommerce',
+  },
+};
+
+export const Admin = {
+  args: {
+    type: ELogoType.NOBORDER,
+    text: '',
+  },
+};
