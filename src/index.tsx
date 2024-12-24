46222bd565ea3ac54bc338c67d7f026ff4976e38
Egor Levchenko <antytoto@gmail.com>
2024-12-24 15:30:19 +0300

    fix: delete class

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './app/index.css';
-import './styles/styles.scss';
 import App from './app/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
