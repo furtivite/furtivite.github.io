469c10c1539f3dc4a452f60774ececc987035ce2
Egor Levchenko <antytoto@gmail.com>
2025-01-15 23:59:16 +0300

    fix: change tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,30 +2,31 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,html}'],
   theme: {
-    colors: {
-      b: {
-        100: '#E6E7E8',
-        200: '#B6B7BC',
-        500: '#5C5F6A',
-        600: '#474B57',
-        800: '#202533',
-        900: '#0E1422',
-      },
-      w: {
-        100: '#F6F6F6',
-        900: '#FFFFFF',
-      },
-      blue: {
-        400: '#A3BEF8',
-      },
-      yellow: {
-        400: '#FFD58A',
-      },
-      green: {
-        400: '#83B18B',
+    extend: {
+      colors: {
+        b: {
+          100: '#E6E7E8',
+          200: '#B6B7BC',
+          500: '#5C5F6A',
+          600: '#474B57',
+          800: '#202533',
+          900: '#0E1422',
+        },
+        w: {
+          100: '#F6F6F6',
+          900: '#FFFFFF',
+        },
+        blue: {
+          400: '#A3BEF8',
+        },
+        yellow: {
+          400: '#FFD58A',
+        },
+        green: {
+          400: '#83B18B',
+        },
       },
     },
-    extend: {},
     fontFamily: {
       inter: ['Inter', 'sans-serif'],
       manrope: ['Manrope', 'sans-serif'],
