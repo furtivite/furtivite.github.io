ba5ec271d0683061c46a40c20cf861ee9a4a64e6
Egor Levchenko <antytoto@gmail.com>
2025-03-05 10:01:51 +0300

    feat: impruve type of Btn

--- src/shared/Btns/interfaces.ts
+++ src/shared/Btns/interfaces.ts
@@ -3,6 +3,7 @@ import React from 'react';
 export interface IBtnDefault {
   isDisabled?: boolean;
   onClick: () => void;
+  type?: 'button' | 'submit' | 'reset';
 }
 
 export interface IBtn extends IBtnDefault {
